3cb80621fb8ffc190ab8797abb824e70a2c95399
Neil Sweeney <neil@wolfiezero.com>
2018-07-13 10:38:25 +0100

    Add venue by season id endpoint

--- tests/Requests/VenueTest.php
+++ tests/Requests/VenueTest.php
@@ -17,4 +17,11 @@ class VenueTest extends TestCase {
         $this->assertEquals($this->venueId, $response->data->id);
     }
 
+    public function it_retrieves_a_venue_by_season_id()
+    {
+        $response = SoccerAPI::venues()->bySeasonId($this->seasonId);
+
+        $this->assertEquals($this->venueId, $response->data->id);
+    }
+
 }
